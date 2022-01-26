e5c13792167b6ea89eda4ad30f8d8230c00b58b2
Cesar Casadiego <cesarmiguel199@gmail.com>
2022-01-26 10:01:36 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import Notify from './notify';
 const DashboardScreen = lazy(() => import('./screens/DashboardScreen'));
-import {dataDownload} from './hooks/dataDownload';
 
 const queryClient = new QueryClient();
 
@@ -40,18 +39,10 @@ const theme = createTheme({
 
 const App = () => {
 
-  // const {download,descomprimir,downloadstatus,downloadpercentage,downloadbytes,downloadtotalbytes,decompresstatus} = dataDownload();
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <QueryClientProvider client={queryClient} contextSharing={true} >
-      {/* <div>{downloadstatus}</div>
-      <div>{downloadpercentage}</div>
-      <div>{downloadbytes}</div>
-      <div>{downloadtotalbytes}</div>
-      <div>{decompresstatus}</div>
-      <button onClick={download}>descargar</button>
-      <button onClick={descomprimir}>descomprimir</button> */}
         <ThemeProvider theme={theme}>
           <DashboardScreen />
           <Notify />
